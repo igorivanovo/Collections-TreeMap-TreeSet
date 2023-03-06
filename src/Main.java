dc27bb7129911a8ccaeed2ad58d37e9eca9e2244
Igor Arhipov <igor.arhipov.69@mail.ru>
2023-03-06 12:31:20 +0300

    Изменена логика compare

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,22 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<Person> people = new ArrayList<>();
-        people.add(new Person("Игорь", "Архипов", 53));
-        people.add(new Person("Филипп", "Воронов", 30));
+        people.add(new Person("Игорь", "Архипов-Иваново", 53));
+        people.add(new Person("Филипп", "Воронов-Netology-эксперт", 30));
         people.add(new Person("Алёна", "Загрекова", 26));
-        people.add(new Person("Георгий", "Власов", 25));
-        people.add(new Person("Максим", " Водолажский", 33));
+        people.add(new Person("Георгий", "Власов-Аспирант", 25));
+        people.add(new Person("Максим", "Водолажский", 33));
         System.out.println(people);
 
-        Collections.sort(people, new PersonsSuNameLengthComparator(20));
-        System.out.println("По длине фамилии, потом по возрасту -  " + people);
+        Collections.sort(people, new PersonsSuNameLengthComparator(1));
+        System.out.println(" По возрасту -  " + people);
 
-        Collections.sort(people, new PersonsSuNameLengthComparator(5));
-        System.out.println("Есть ограничение по длине фамилии -  " + people);
+        Collections.sort(people, new PersonsSuNameLengthComparator(2));
+        System.out.println(" Есть ограничение на количество фамилий -  " + people);
+
+        Collections.sort(people, new PersonsSuNameLengthComparator(3));
+        System.out.println(" По длине фамилии , потом по возрасту - " + people);
+
+
     }
 }
--- src/PersonsSuNameLengthComparator.java
+++ src/PersonsSuNameLengthComparator.java
@@ -1,18 +1,21 @@
+import java.awt.*;
 import java.util.Comparator;
 
 public class PersonsSuNameLengthComparator implements Comparator<Person> {
-    protected int lengthSuName;
+    protected int quantitySuName;
 
-    public PersonsSuNameLengthComparator(int lengthSuName) {
-        this.lengthSuName = lengthSuName;
+    public PersonsSuNameLengthComparator(int quantitySuName) {
+        this.quantitySuName = quantitySuName;
     }
 
     @Override
     public int compare(Person o1, Person o2) {
-        if (o1.getSurname().length() > lengthSuName & o2.getSurname().length() > lengthSuName) {
+        String[] text1 = o1.getSurname().split("\\P{IsAlphabetic}+");
+        String[] text2 = o2.getSurname().split("\\P{IsAlphabetic}+");
+        if (text1.length >= quantitySuName & text2.length >= quantitySuName) {
             return Integer.compare(o1.getAge(), o2.getAge());
         }
-        if (o1.getSurname().length() < o2.getSurname().length()) {
+        if ((o1.getSurname().length() - text1.length + 1) < (o2.getSurname().length()) - text2.length + 1) {
             return -1;
         } else if (o1.getSurname().length() > o2.getSurname().length()) {
             return 1;
