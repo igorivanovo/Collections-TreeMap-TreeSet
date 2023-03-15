ece67750ebf7393121e87646e0c0e5da4181e0a3
Igor Arhipov <igor.arhipov.69@mail.ru>
2023-03-15 22:38:11 +0300

    Added Predicate<Person>

--- src/PersonsSuNameLengthComparator.java
+++ src/PersonsSuNameLengthComparator.java
@@ -14,13 +14,12 @@ public class PersonsSuNameLengthComparator implements Comparator<Person> {
         String[] text2 = o2.getSurname().split("\\P{IsAlphabetic}+");
         if (text1.length >= quantitySuName & text2.length >= quantitySuName) {
             return Integer.compare(o1.getAge(), o2.getAge());
-        }
-        if ((o1.getSurname().length() - text1.length + 1) < (o2.getSurname().length()) - text2.length + 1) {
-            return -1;
-        } else if (o1.getSurname().length() > o2.getSurname().length()) {
-            return 1;
-        } else {
+        } else if (text1.length == text2.length) {
             return Integer.compare(o1.getAge(), o2.getAge());
-        }
+        } else if (text1.length < text2.length) {
+            return -1;
+        } else return 1;
+
     }
 }
+
